427c1c3aabf0c62326b40204bc7702f2dfc38d6c
Chandru Narayan <54154611+chandrunarayan@users.noreply.github.com>
2023-02-03 20:14:32 -0800

    updates

--- jupyter/entrypoint.sh
+++ jupyter/entrypoint.sh
@@ -4,6 +4,10 @@ UDEV=1
 
 echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 udevadm control --reload
+cp -f /usr/src/app/*.ipynb /data
+cp -f /usr/src/app/*.py /data
+ln -s /data /work/notebooks/data
+
 libcamera-hello --list-cameras -n -v
 libcamera-still -n -o /data/start.jpg
 jupyter lab --notebook-dir=/work/notebooks --port=8081 --no-browser --ip=0.0.0.0 --allow-root
