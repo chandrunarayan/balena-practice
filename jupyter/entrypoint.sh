4aba4e4cda8a154e67321bdb87723f514b3ae24d
Chandru Narayan <54154611+chandrunarayan@users.noreply.github.com>
2023-03-04 20:07:54 -0800

    Update entrypoint.sh

--- jupyter/entrypoint.sh
+++ jupyter/entrypoint.sh
@@ -7,7 +7,6 @@ printf "Host IP is: $(hostname -I|cut -d " " -f1)\n"
 udevadm control --reload
 cp -f /usr/src/app/*.ipynb /data
 cp -f /usr/src/app/*.py /data
-ln -s /data /work/notebooks/data
 
 libcamera-hello --list-cameras -n -v
 libcamera-still -n -o /data/start.jpg
